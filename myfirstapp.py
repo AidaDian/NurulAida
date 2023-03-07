4c1284da911b5b225fd86810b077ffd58bd74d5d
AidaDian <127168660+AidaDian@users.noreply.github.com>
2023-03-07 12:27:17 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -5,9 +5,6 @@ import time
 
 st.header("My first Streamlit App")
 
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox('I agree the terms and conditions')
-
 option = st.sidebar.selectbox(
     'Select a mini project',
      ['line chart','map','T n C','Long Process'])
@@ -25,8 +22,8 @@ elif option=='map':
     columns=['lat', 'lon'])
 
     st.map(map_data)
-    
- elif option=='T n C':
+
+elif option=='T n C':
     st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
     show = st.checkbox('I agree the terms and conditions')
     if show:
@@ -48,5 +45,4 @@ else:
         time.sleep(0.1)
 
     '...and now we\'re done!'
-
 
